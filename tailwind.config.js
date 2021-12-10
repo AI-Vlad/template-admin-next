ac3fd6bbb3dcf3c157377b268deba0b53d87075c
Vladimir Andre Rojas <vrojas@FAZENDA.MS>
2021-12-10 15:42:25 -0400

    adicionando tema dark no projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/components/template/**/*.{js,ts,jsx,tsx}'],
+  purge:{
+    enabled: process.env.PURGE_CSS === 'production' ? true : false,
+    content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/components/template/**/*.{js,ts,jsx,tsx}']
+  },
+ 
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
